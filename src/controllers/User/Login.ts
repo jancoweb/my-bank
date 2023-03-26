ca9036d7ef676329a33f0af7c50a97f80ef4e9db
jancoweb <jancoweb@outlook.com>
2023-03-26 04:47:48 -0300

    auth com jwt

--- src/controllers/User/Login.ts
+++ src/controllers/User/Login.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
@@ -7,20 +8,29 @@ const prisma = new PrismaClient()
 export class UserLogin {
   async Login(req: Request, res: Response) {
     const { email, password } = req.body;
-    const getAc = await prisma.user.findFirst({
+    if (!email || !password) return res.status(404).json({ error: "É necessário preencher todos os campos" });
+
+    const userLogged = await prisma.user.findFirst({
       where: {
         email
       }
-    })
-    const hashPw = getAc ? getAc.password : '';
+    });
+    const hashPw = userLogged ? userLogged.password : '';
 
-    const isMatch = await bcrypt.compare(password, hashPw)
+    const isMatch = await bcrypt.compare(password, hashPw);
 
-    if (isMatch === false) {
-      return res.status(401).json({ error: "Email ou senha incorretos." })
-    }
+    if (isMatch === false) return res.status(401).json({ error: "Email ou senha incorretos." });
 
-    // AUTH COM JWT
+    const jwtsecret: string = process.env.JWT_SECRET ?? '';
+    const token = jwt.sign({
+      id: userLogged?.id
+    }, jwtsecret)
+    const user = {
+      firstName: userLogged?.firstName,
+      email: userLogged?.email
+    };
 
+    const response = { user, token };
+    return res.status(200).json(response)
   }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,11 @@ import { CreateUserController } from './controllers/User/CreateUserController'
 import { UserLogin } from './controllers/User/Login';
 
 const createUserController = new CreateUserController();
-const login = new UserLogin();
+const signin = new UserLogin();
 
 const userRoutes = Router();
 
 userRoutes.post('/signup', createUserController.handle)
-userRoutes.post('/signin', login.Login)
+userRoutes.post('/signin', signin.Login)
 
 export { userRoutes }
