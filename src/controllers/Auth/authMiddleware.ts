911a217531bafabedabf3c2c8e038e3215c67faa
jancoweb <jancoweb@outlook.com>
2023-03-26 06:22:55 -0300

    auth com jwt

---
+++ src/controllers/Auth/authMiddleware.ts
@@ -0,0 +1,45 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from 'jsonwebtoken';
+
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient()
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: number,
+        email: string,
+        phone: string,
+        address: string
+      }
+    }
+  }
+}
+
+export class Auth {
+  async validate(req: Request, res: Response, next: NextFunction) {
+    const { authorization } = req.headers;
+    if (!authorization) return res.status(401).json({ error: "Auth inválida" })
+    const token = authorization.split(" ")[1];
+
+    try {
+      const jwtsecret = process.env.JWT_SECRET ?? '';
+      const data = jwt.verify(token, jwtsecret);
+      if (typeof (data) !== 'object') return res.status(500)
+
+      const findUser = await prisma.user.findFirst({
+        where: {
+          id: data.id
+        }
+      })
+      if (!findUser) return res.status(404).json({ error: "Usuário não encontrado" });
+
+      const { password, ...user } = findUser;
+      req.user = user
+      next();
+    } catch (error) {
+      return res.status(401).json({ error: "Token inválido" })
+    }
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express'
+import { Auth } from './controllers/Auth/authMiddleware';
 import { CreateUserController } from './controllers/User/CreateUserController'
 import { UserLogin } from './controllers/User/Login';
 
 const createUserController = new CreateUserController();
 const signin = new UserLogin();
+const auth = new Auth();
 
 const userRoutes = Router();
 
 userRoutes.post('/signup', createUserController.handle)
 userRoutes.post('/signin', signin.Login)
+
+userRoutes.use(auth.validate)
 
 export { userRoutes }
